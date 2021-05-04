87c351f5fa20361f26a72a16e8369355e4dbd29f
LeonardoFrazzetto <leonardo.frazzetto1990@gmail.com>
2021-05-04 21:20:22 +0200

    Week4 Dia1

--- week3_course_python_III/day5_probability_statistics/exercise/tablero1.py
+++ week3_course_python_III/day5_probability_statistics/exercise/tablero1.py
@@ -1,5 +1,18 @@
-class Tablero:
-    def __init__(self, ancho = 10, alto = 10):
-
-    def construir_tablero():
-        
+ListasAlumnos = [['Juan', 'Carmelo', 5, 7, 9, 7], ['Laura', 'Jazmine', 7, 8, 5, 6.66],
+ ['Dario', 'Villalobos', 5, 6, 3, 4.66], ['Marito', 'Tasolo', 4, 7, 9, 6.666],
+  ['Esteban', 'Quito', 9, 9, 8, 8.66]]
+Tabla = """\
++---------------------------------------------------------------------+
+| Nombre    Apellido        Primero Segundo Tercero     Promedio anual|
+|---------------------------------------------------------------------|
+{}
++---------------------------------------------------------------------+\
+"""
+Tabla1 = (Tabla.format('\n'.join("| {:<8} {:<10} {:>8} {:>6} {:>7} {:>23} |".format(*fila)
+ for fila in ListasAlumnos)))
+print (Tabla1)
+ListasAlumnos[1][0] = '#'
+print(ListasAlumnos)
+Tabla2 = (Tabla.format('\n'.join("| {:<8}| {:<10}| {:>8}| {:>6}| {:>7}| {:>18} |".format(*fila)
+ for fila in ListasAlumnos)))
+print (Tabla2)
